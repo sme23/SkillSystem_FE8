813e92838ccf3eb74feb8989efd6e8317c76d0ca
Sme <esahig15789@gmail.com>
2023-08-03 21:59:01 -0400

    New Wrath
    Adds the new functionality for Wrath, and removes the old functionality. Also adds a handful of helper functions for parsing battle rounds.

---
+++ EngineHacks/Necessary/CalcLoops/BattleProcCalcLoop/BattleRoundParsing/BattleRoundParsing.c
@@ -0,0 +1,36 @@
+#include "BattleRoundParsing.h"
+
+bool isFirstRound(NewBattleHit* round) {
+	return (round->info & BATTLE_HIT_INFO_BEGIN);
+}
+
+bool isLastRound(NewBattleHit* round) {
+	return (round->info & BATTLE_HIT_INFO_END);
+}
+
+bool isDefenderRound(NewBattleHit* round) {
+	return (round->info & BATTLE_HIT_INFO_RETALIATION);
+}
+
+bool roundHits(NewBattleHit* round) {
+	return !(round->attributes & BATTLE_HIT_ATTR_MISS);
+}
+
+bool roundCrits(NewBattleHit* round) {
+	return (round->attributes & BATTLE_HIT_ATTR_CRIT);
+}
+
+bool isFollowupAttack(NewBattleHit* round) {
+	return (round->attributes & BATTLE_HIT_ATTR_FOLLOWUP);
+}
+
+//NOTE: this function may not work as expected on the first iteration of the BattleProcCalcLoop
+NewBattleHit* getFirstDefenderRound(NewBattleHit* round) {
+	//Iterate through rounds until we find one marked as last
+	while(true) {
+		if (isDefenderRound(round)) return round;
+		if (isLastRound(round)) return 0;
+		round++;
+	}
+}
+
