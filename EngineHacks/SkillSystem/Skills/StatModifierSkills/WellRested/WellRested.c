2b04976ed84ed24960afd73268f21090d8cb79c9
Sme <esahig15789@gmail.com>
2023-09-01 02:11:22 -0400

    well-rested functional

--- EngineHacks/SkillSystem/Skills/StatModifierSkills/WellRested/WellRested.c
+++ EngineHacks/SkillSystem/Skills/StatModifierSkills/WellRested/WellRested.c
@@ -6,39 +6,76 @@ u32 WellRested(u32 stat, Unit* unit) {
 	if (SkillTester(unit,WellRestedIDLink)) {
 		//add our bonus value to the stat
 		stat += WellRested_GetBonus(unit);
+		//if we were deployed last time, -1 to bonus at prep
+		if (!WellRested_CheckUndeployedFlag(unit) && (gChapterData.chapterStateBits & 0x10)) stat--;
+		
 	}
 	return stat;
 }
 
 u8 WellRested_GetBonus(Unit* unit) {
-	//given a unit, get their current bonus
-	u32* debuffEntry = GetUnitDebuffEntry(unit);
-	int val = UnpackData_Signed(debuffEntry, WellRestedBitOffset_Link, 5);
-	return val;
+	//get the current well-rested bonus
+	return WellRestedRAMLoc->wellRestedBoost;
 }
 
 void WellRested_IncrementBonus(Unit* unit) {
-	u32* debuffEntry = GetUnitDebuffEntry(unit);
-	int val = UnpackData_Signed(debuffEntry, WellRestedBitOffset_Link, 5);
+	int val = WellRestedRAMLoc->wellRestedBoost;
 	val++;
 	if (val > 15) val = 15;
-	//PackData_Signed(debuffEntry, WellRestedBitOffset_Link, 5, val);
+	WellRestedRAMLoc->wellRestedBoost = val;	
 }
 
 void WellRested_DecrementBonus(Unit* unit) {
-	u32* debuffEntry = GetUnitDebuffEntry(unit);
-	int val = UnpackData_Signed(debuffEntry, WellRestedBitOffset_Link, 5);
+	int val = WellRestedRAMLoc->wellRestedBoost;
 	val--;
-	u8 baseMov = unit->pClassData->baseMov;
-	if (val < baseMov) val = baseMov;
-	//PackData_Signed(debuffEntry, WellRestedBitOffset_Link, 5, val);	
+	if (val < 0) val = 0;
+	WellRestedRAMLoc->wellRestedBoost = val;	
 }
 
-void PrePrep_WellRestedBonus(Unit* unit) {
-	//for each unit, if they have WellRested, either increment or decrement their bonus
+bool WellRested_CheckAndUnsetUndeployedFlag(Unit* unit) {
+	bool retVal = WellRestedRAMLoc->wellRestedFlag;
+	WellRestedRAMLoc->wellRestedFlag = 0;
+	return retVal;
+}
+
+bool WellRested_CheckUndeployedFlag(Unit* unit) {
+	return WellRestedRAMLoc->wellRestedFlag;
+}
+
+void WellRested_SetUndeployedFlag(Unit* unit) {
+	WellRestedRAMLoc->wellRestedFlag = 1;
+}
+
+void PostPrep_WellRestedBonus() {
+	//for each player unit, if they have WellRested, either increment or decrement their bonus
 	
-	if (SkillTester(unit, WellRestedIDLink)) {
-		if (unit->state & US_NOT_DEPLOYED) { WellRested_IncrementBonus(unit); }
-		else { WellRested_DecrementBonus(unit); }
+	for (int i = 1; i < 64; i++) {
+		
+		Unit* unit = GetUnit(i);
+		if (unit->maxHP == 0) continue;
+	
+		if (SkillTester(unit, WellRestedIDLink)) {
+			if (unit->state & US_NOT_DEPLOYED) { 
+			//we weren't deployed, so set a bit to denote this
+			WellRested_SetUndeployedFlag(unit);
+			//then increment our bonus
+			WellRested_IncrementBonus(unit); 
+			}
+			else { 
+			//if we were not deployed last chapter, do nothing
+			if (WellRested_CheckAndUnsetUndeployedFlag(unit)) continue;
+			//otherwise, decrement the bonus
+			WellRested_DecrementBonus(unit); }
+		}
 	}
+}
+
+WellRestedData* WellRested_SaveByte(WellRestedData* saveData) {
+	saveData->wellRestedBoost = WellRestedRAMLoc->wellRestedBoost;
+	saveData->wellRestedFlag = WellRestedRAMLoc->wellRestedFlag;
+}
+
+Unit* WellRested_LoadByte(WellRestedData* saveData) {
+	WellRestedRAMLoc->wellRestedBoost = saveData->wellRestedBoost;
+	WellRestedRAMLoc->wellRestedFlag = saveData->wellRestedFlag;	
 }
--- EngineHacks/SkillSystem/Skills/StatModifierSkills/WellRested/WellRested.h
+++ EngineHacks/SkillSystem/Skills/StatModifierSkills/WellRested/WellRested.h
@@ -8,11 +8,35 @@ extern void PackData_Signed(u32* address, u8 bitOffset, u8 bitCount, u8 value);
 extern int UnpackData_Signed(u32* address, u8 bitOffset, u8 bitCount);
 extern u32* GetUnitDebuffEntry(Unit* unit);
 
+struct BWLData {
+/*00*/  u8 losses;
+/*01*/  u8 skills[4];
+/*05*/  u8 mostRecentDeathChapter : 4;
+/*05*/	u16 mostRecentDeathTurn : 12;
+/*07*/	u8 unused1;
+/*08*/	u8 unused2;
+/*09*/	u8 pad[7]; //irrelevant for here
+} typedef BWLData;
+
+extern BWLData gBWLDataArray[];
+
+struct WellRestedData {
+	u8 wellRestedBoost;
+	u8 wellRestedFlag;
+} typedef WellRestedData;
+
 extern u8 WellRestedIDLink;
 extern u8 WellRestedBitOffset_Link;
+extern WellRestedData* WellRestedRAMLoc;
 
 u32 WellRested(u32 stat, Unit* unit); 
 u8 WellRested_GetBonus(Unit* unit);
 void WellRested_IncrementBonus(Unit* unit);
 void WellRested_DecrementBonus(Unit* unit);
-void PrePrep_WellRestedBonus(Unit* unit);
+void PostPrep_WellRestedBonus();
+bool WellRested_CheckAndUnsetUndeployedFlag(Unit* unit);
+void WellRested_SetUndeployedFlag(Unit* unit);
+bool WellRested_CheckUndeployedFlag(Unit* unit);
+BWLData* UnitGetBWLData(Unit* unit);
+WellRestedData* WellRested_SaveByte(WellRestedData* saveData);
+Unit* WellRested_LoadByte(WellRestedData* saveData);
