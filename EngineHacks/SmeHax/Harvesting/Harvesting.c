7d03e00342b601c365e625060baefac2aecf63e6
Sme <esahig15789@gmail.com>
2023-11-29 00:27:58 -0500

    h

--- EngineHacks/SmeHax/Harvesting/Harvesting.c
+++ EngineHacks/SmeHax/Harvesting/Harvesting.c
@@ -11,3 +11,44 @@ u8 PickHarvestResult(HarvestResult* resultList) {
 	
 }
 
+void InitalizeHarvestToolLevels() {
+	for (int i = 0; i < 16; i++) {
+		*HarvestToolTiers[i] = 0;
+	}
+}
+
+void SetHarvestToolLevel(int tool, int level) {
+	*HarvestToolTiers[tool] = level;
+}
+
+void SetHarvestToolLevelIfHigher(int tool, int level) {
+	if (level > *HarvestToolTiers[tool]) *SetHarvestToolLevel[tool] = level;
+}
+
+int GetHarvestToolLevel(int tool) {
+	return *HarvestToolTiers[tool];
+}
+
+void BuildToolList(u16* listPtr, Trap* harvestTile) {
+	// for each tool,
+	// if its level is >0, 
+	// and the harvest set has a nonzero pointer, 
+	// add to list and increment list pointer
+	
+	
+	
+}
+
+void AddHarvestTile_ASMC(Proc* parent) {
+	int x = gEventSlot[0xB] & 0xFFFF;
+	int y = gEventSlot[0xB] >> 16;
+	Trap* trap = AddTrap(x, y, TRAP_HARVEST_TILE, 0);
+	
+	trap->data[TRAP_EXTDATA_HARVEST_TILE_SET_ID] = gEventSlot[1];
+	trap->data[TRAP_EXTDATA_HARVEST_TILE_HARVESTED] = 0;
+}
+
+
+
+
+
--- EngineHacks/SmeHax/Harvesting/Harvesting.h
+++ EngineHacks/SmeHax/Harvesting/Harvesting.h
@@ -7,15 +7,40 @@ struct HarvestResult {
 } typedef HarvestResult;
 
 struct HarvestSet {
-	HarvestResult* toolResult[256];
+	HarvestResult* toolResult[16];
 } typedef HarvestSet;
 
 struct HarvestTable {
 	HarvestSet* set[256];
 } typedef HarvestTable;
 
-//EA literals
+enum {
+	TOOL_STAFF 			= 0, 	// hit things to knock off materials
+	TOOL_SCYTHE 		= 1, 	// cut off materials
+	TOOL_AXE 			= 2, 	// break things open, take them apart wholly
+	TOOL_BUG_NET 		= 3, 	// catch bugs on thing
+	TOOL_FISHING_ROD	= 4, 	// catch fish on thing
+	TOOL_EXPLOSIVES	 	= 5, 	// blow it up, fire transmutation
+	TOOL_ICE_CHARGE		= 6, 	// freeze it, ice transmutation
+	TOOL_PINWHEEL		= 7, 	// blow at it, wind transmutation
+	TOOL_LIGHTNING_ROD	= 8,	// electrify it, thunder transmutation
+	TOOL_SPLASH_BOTTLE 	= 9, 	// make it wet, water transmutation
+	TOOL_FERTILIZER		= 10,	// magically grow it, earth transmutation
+	TOOL_LIGHTBULB 		= 11, 	// infuse with light, light transmutation
+	TOOL_DARKBULB 		= 12, 	// infuse with darkness, dark transmutation
+	//there's room for 16 of these if needed
 
+	TRAP_HARVEST_TILE	= 20,
+	
+	TRAP_EXTDATA_HARVEST_TILE_SET_ID 	= 0, // ID 
+	TRAP_EXTDATA_HARVEST_TILE_HARVESTED = 1
+	
+}
+
+//EA literals
+extern u8* HarvestToolTiers[16];
 
 //function prototypes
 u8 PickHarvestResult(HarvestResult* resultList);
+void InitalizeHarvestToolLevels();
+
