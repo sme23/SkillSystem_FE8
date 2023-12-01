d6df6754340b925788baa5722721f31ccea05045
Sme <esahig15789@gmail.com>
2023-11-30 22:26:34 -0500

    more harvesting stuff

--- EngineHacks/SmeHax/Harvesting/Harvesting.c
+++ EngineHacks/SmeHax/Harvesting/Harvesting.c
@@ -22,11 +22,16 @@ void SetHarvestToolLevel(int tool, int level) {
 }
 
 void SetHarvestToolLevelIfHigher(int tool, int level) {
-	if (level > *HarvestToolTiers[tool]) *SetHarvestToolLevel[tool] = level;
+	if (level > *HarvestToolTiers[tool]) SetHarvestToolLevel(tool, level);
 }
 
 int GetHarvestToolLevel(int tool) {
 	return *HarvestToolTiers[tool];
+}
+
+HarvestResult* GetHarvestResult(int id, int tool) {
+	HarvestSet* e = HarvestingTable.set[id];
+	return e->toolResult[tool];
 }
 
 void BuildToolList(u16* listPtr, Trap* harvestTile) {
@@ -35,7 +40,17 @@ void BuildToolList(u16* listPtr, Trap* harvestTile) {
 	// and the harvest set has a nonzero pointer, 
 	// add to list and increment list pointer
 	
+	int tool = harvestTile->data[0];
 	
+	for (u16 i = 0; i < 16; i++) {
+		if (!GetHarvestToolLevel(i)) continue;
+		if (!GetHarvestResult(i, tool)) continue;
+		
+		*listPtr = i;
+		listPtr++;		
+	}
+	
+	*listPtr = 0;
 	
 }
 
@@ -47,6 +62,83 @@ void AddHarvestTile_ASMC(Proc* parent) {
 	trap->data[TRAP_EXTDATA_HARVEST_TILE_SET_ID] = gEventSlot[1];
 	trap->data[TRAP_EXTDATA_HARVEST_TILE_HARVESTED] = 0;
 }
+
+
+u8 HarvestMenuHelpBox(MenuProc* menu, MenuCommandProc* menuItem) {
+	//runs when the help box is being drawn.
+	//responsible for making the help box for this menu
+	
+	int tool = *HarvestToolBuffer[menuItem->commandDefinitionIndex - 1];
+	
+	StartHarvestHelpBox(menuItem->xDrawTile << 3, menuItem->yDrawTile << 3, tool);
+	
+	return 0;
+	
+}
+
+void StartHarvestHelpBox(int x, int y, int tool) {
+	gSomeTextRelatedStuff.adjUp    = NULL;
+    gSomeTextRelatedStuff.adjDown  = NULL;
+    gSomeTextRelatedStuff.adjLeft  = NULL;
+    gSomeTextRelatedStuff.adjRight = NULL;
+
+    gSomeTextRelatedStuff.xDisplay = x;
+    gSomeTextRelatedStuff.yDisplay = y;
+    gSomeTextRelatedStuff.mid      = tool;
+
+    gSomeTextRelatedStuff.redirect = NULL;
+    gSomeTextRelatedStuff.populate = HbPopulate_AutoTool;
+
+    gHelpTextOriginTile.x = 0;
+    gHelpTextOriginTile.y = 0;
+
+    StartHelpBoxExt(&gSomeTextRelatedStuff, FALSE);
+}
+
+void HbPopulate_AutoTool(HelpBoxProc* proc) {
+	//this will eventually be what's responsible for picking the thing to display
+	//which will be from a table based on the tool ID which is in proc->info->mid
+}
+
+u8 HarvestMenu_ButtonBPressed(MenuProc* menu, MenuCommandProc* menuItem) {
+	FillBgMap(gBg2MapBuffer, 0);
+    EnableBgSyncByMask(BG2_SYNC_BIT);
+
+    Text_ResetTileAllocation();
+
+    StartMenu_AndDoSomethingCommands(&gMenu_UnitMenu, gGameState.cameraRealPos.x - gGameState.cameraRealPos.y, 1, 22);
+
+    HideMoveRangeGraphics();
+	
+	 return ME_DISABLE | ME_END | ME_PLAY_BOOP | ME_CLEAR_GFX | ME_END_FACE0;
+}
+
+
+u8 HarvestMenu_Usability(const MenuCommandDefinition* command, int number) {
+	//returns 1 if the # has a tool, otherwise returns 3
+	return 1;
+}
+int HarvestMenu_TextDraw(MenuProc* menu, MenuCommandProc* menuItem) {
+	int tool = *HarvestToolBuffer[menuItem->commandDefinitionIndex - 1];
+
+	//DrawItemMenuLine is not going to do the thing that its supposed to, it will draw the item of the tool's ID. need to copy this later
+    DrawItemMenuLine(&menuItem->text, tool, 1, gBg0MapBuffer + TILEMAP_INDEX(menuItem->xDrawTile, menuItem->yDrawTile));
+
+    return 0;
+}
+
+u8 HarvestMenu_Effect(MenuProc* menu, MenuCommandProc* menuItem) {
+	//roll the harvest result, do a popup for it, set the action to wait
+}
+
+int HarvestMenu_SwitchIn(MenuProc* menu, MenuCommandProc* menuItem) {
+	//runs when you move the cursor and the menuItem index is the newly selected option
+}
+
+int HarvestMenu_SwitchOut(MenuProc* menu, MenuCommandProc* menuItem) {
+	//runs when you move the cursor and the menuItem index is the previously selected option
+}
+
 
 
 
--- EngineHacks/SmeHax/Harvesting/Harvesting.h
+++ EngineHacks/SmeHax/Harvesting/Harvesting.h
@@ -1,5 +1,67 @@
 #include "gbafe.h"
 
+//lib stuff
+struct HelpBoxInfo;
+
+struct HelpBoxProc
+{
+    /* 00 */ PROC_HEADER;
+
+    /* 2C */ const struct HelpBoxInfo* info;
+
+    /* 30 */ short xBox;
+    /* 32 */ short yBox;
+    /* 34 */ short wBox;
+    /* 36 */ short hBox;
+    /* 38 */ short xBoxInit;
+    /* 3A */ short yBoxInit;
+    /* 3C */ short xBoxFinal;
+    /* 3E */ short yBoxFinal;
+    /* 40 */ short wBoxInit;
+    /* 42 */ short hBoxInit;
+    /* 44 */ short wBoxFinal;
+    /* 46 */ short hBoxFinal;
+    /* 48 */ short timer;
+    /* 4A */ short timerMax;
+
+    /* 4C */ u16 mid;
+    /* 4E */ u16 item;
+
+    /* 50 */ u16 moveKey; // move ctrl proc only
+
+    /* 52 */ u8 unk52;
+
+    // NOTE: there's likely more, need to decompile more files
+} typedef HelpBoxProc;
+
+
+struct HelpBoxInfo
+{
+    /* 00 */ const struct HelpBoxInfo* adjUp;
+    /* 04 */ const struct HelpBoxInfo* adjDown;
+    /* 08 */ const struct HelpBoxInfo* adjLeft;
+    /* 0C */ const struct HelpBoxInfo* adjRight;
+    /* 10 */ u8 xDisplay;
+    /* 11 */ u8 yDisplay;
+    /* 12 */ u16 mid;
+    /* 14 */ void(*redirect)(struct HelpBoxProc* proc);
+    /* 18 */ void(*populate)(struct HelpBoxProc* proc);
+} typedef HelpBoxInfo;
+
+extern HelpBoxInfo gSomeTextRelatedStuff;
+
+extern struct Vec2 gHelpTextOriginTile;
+
+extern void StartHelpBoxExt(const HelpBoxInfo* info, int unk);
+extern MenuProc* StartMenu_AndDoSomethingCommands(const MenuDefinition* def, int xSubject, int xTileLeft, int xTileRight);
+
+extern MenuDefinition gMenu_UnitMenu;
+
+#define TILEMAP_INDEX(aX, aY) (0x20 * (aY) + (aX))
+
+
+
+
 //structs
 struct HarvestResult {
   u8 resultID;
@@ -35,12 +97,29 @@ enum {
 	TRAP_EXTDATA_HARVEST_TILE_SET_ID 	= 0, // ID 
 	TRAP_EXTDATA_HARVEST_TILE_HARVESTED = 1
 	
-}
+};
 
 //EA literals
 extern u8* HarvestToolTiers[16];
+extern HarvestTable HarvestingTable;
+extern u16* HarvestToolBuffer[16];
 
 //function prototypes
 u8 PickHarvestResult(HarvestResult* resultList);
 void InitalizeHarvestToolLevels();
+void SetHarvestToolLevel(int tool, int level);
+void SetHarvestToolLevelIfHigher(int tool, int level);
+int GetHarvestToolLevel(int tool);
+HarvestResult* GetHarvestResult(int id, int tool);
+void BuildToolList(u16* listPtr, Trap* harvestTile);
+void AddHarvestTile_ASMC(Proc* parent);
+u8 HarvestMenuHelpBox(MenuProc* menu, MenuCommandProc* menuItem);
+u8 HarvestMenu_ButtonBPressed(MenuProc* menu, MenuCommandProc* menuItem);
+u8 HarvestMenu_Usability(const MenuCommandDefinition* command, int number);
+int HarvestMenu_TextDraw(MenuProc* menu, MenuCommandProc* menuItem);
+u8 HarvestMenu_Effect(MenuProc* menu, MenuCommandProc* menuItem);
+int HarvestMenu_SwitchIn(MenuProc* menu, MenuCommandProc* menuItem);
+int HarvestMenu_SwitchOut(MenuProc* menu, MenuCommandProc* menuItem);
+void StartHarvestHelpBox(int x, int y, int tool);
+void HbPopulate_AutoTool(HelpBoxProc* proc);
 
