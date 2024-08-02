9749e863b5277c518842ee8418c4cc0f763d36eb
Sme <esahig15789@gmail.com>
2024-08-02 04:09:16 -0400

    Hand Cannon skill (but for real)

--- EngineHacks/PirateHax/Break/Main/Main.h
+++ EngineHacks/PirateHax/Break/Main/Main.h
@@ -25,6 +25,7 @@ void SetEventId(int eventId);
 extern u8 DazzleIDLink;
 extern u8 ArcaneArtificeIDLink;
 extern u8 PulverizeIDLink;
+extern u8 HandCannonIDLink;
 
 extern bool(*gSkillTester)(Unit* unit, int skillID);
 
